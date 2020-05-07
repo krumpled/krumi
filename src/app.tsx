efeddcc13b90979f4e1037acb18a458d6d719c17
Danny Hadley <dadleyy@gmail.com>
2020-05-07 16:56:42 -0400

    better

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Head from '@krumpled/krumi/components/application-head';
 import Foot from '@krumpled/krumi/components/application-footer';
 import React from 'react';
@@ -7,6 +7,14 @@ export default function run() {
   return (
     <Router>
       <Head />
+        <Switch>
+          <Route path='/login'>
+            I am login
+          </Route>
+          <Route>
+            <Link to='/login'>Login</Link>
+          </Route>
+        </Switch>
       <Foot />
     </Router>
   );
