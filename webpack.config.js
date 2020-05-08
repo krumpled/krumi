6fa2a83e3680ed7b0e819f0cdf48ce63463f7b22
Danny Hadley <dadleyy@gmail.com>
2020-05-08 11:25:12 -0400

    lint fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const html = require('html-webpack-plugin');
+const HTMLPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -9,21 +9,21 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        exclude: /node_modules/
+      }
+    ]
   },
-  plugins: [new html({ template: 'src/index.html' })],
+  plugins: [new HTMLPlugin({ template: 'src/index.html' })],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
-      '@krumpled/krumi': path.resolve(__dirname, 'src'),
-    },
+      '@krumpled/krumi': path.resolve(__dirname, 'src')
+    }
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/'
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
