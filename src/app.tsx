e404ee8a124bb2e58626918af48473a6095127e4
Danny Hadley <dadleyy@gmail.com>
2020-05-08 11:25:12 -0400

    lint fixes

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,21 @@
-import React, { useEffect, useState } from 'react';  
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from 'react-dom';
 import debug from 'debug';
-import { fromNullable } from '@krumpled/krumi/std/option';
 import Header from '@krumpled/krumi/components/application-header';
 import Footer from '@krumpled/krumi/components/application-footer';
 
 const log = debug('krumi:app');
 
 function App(): React.FunctionComponentElement<{}> {
-  const [initial, update] = useState({});
+  const [initial, update] = useState({ count: 0 });
 
   useEffect(function() {
-    log('hello world');
+    log('application main effect - %o', initial);
+
+    if (initial.count === 0) {
+      update({ count: 1 });
+    }
   });
 
   return (
