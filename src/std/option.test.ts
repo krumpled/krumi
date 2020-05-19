097eda0051c45b1985171efdf4769eaa9aa84c8a
Danny Hadley <dadleyy@gmail.com>
2020-05-19 17:09:10 -0400

    changes

--- src/std/option.test.ts
+++ src/std/option.test.ts
@@ -4,8 +4,9 @@ import { describe, expect, it as test } from '@jest/globals';
 describe('Option', function () {
   describe('fromNullable', function () {
     test('correct values', function () {
-      const opt = fromNullable(null);
-      expect(opt.kind).toBe('none');
+      expect(fromNullable(null).kind).toBe('none');
+      expect(fromNullable(undefined).kind).toBe('none');
+      expect(fromNullable('please-dont-be-in-build').kind).toBe('some');
     });
   });
 });
