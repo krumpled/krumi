2ac4bb11e941f81aca967773f0c65dc52b548f2b
Danny Hadley <dadleyy@gmail.com>
2020-06-06 16:06:31 -0400

    v1.1.2 - auth callback + enable

--- src/app.tsx
+++ src/app.tsx
@@ -1,130 +1,84 @@
 import React, { useEffect, useState } from 'react';
-import { Route, Redirect, Switch, BrowserRouter as Router, useLocation } from 'react-router-dom';
+import { Route, Redirect, Switch, BrowserRouter as Router } from 'react-router-dom';
 import { render } from 'react-dom';
-import debug from 'debug';
 import Header from '@krumpled/krumi/components/application-header';
 import Loading from '@krumpled/krumi/components/application-loading';
 import ApplicationError from '@krumpled/krumi/components/application-error';
-import { none, fromNullable } from '@krumpled/krumi/std';
-import { failed, loaded, loading, notAsked, AsyncRequest } from '@krumpled/krumi/std/async-request';
-import { Session, load as loadSession } from '@krumpled/krumi/session';
-import { setContextValue } from '@krumpled/krumi/logging';
+import { none } from '@krumpled/krumi/std';
+import { failed, loaded, loading } from '@krumpled/krumi/std/async-request';
+import { load as loadSession } from '@krumpled/krumi/session';
+import debug, { setContextValue } from '@krumpled/krumi/logging';
 import * as Routes from '@krumpled/krumi/routes';
+import { init, State } from '@krumpled/krumi/app-state';
+import AuthCallback from '@krumpled/krumi/auth-callback';
 
 const log = debug('krumi:app');
 
-type State = {
-  session: AsyncRequest<Session>;
-};
-
-function init(): State {
-  return { session: notAsked() };
-}
-
-// The AuthCallback route handles pulling the token out of the url query params and attempting to load the session
-// using it, immediately calling the update of our `Main` state and rendering a redirect back to home.
-function AuthCallback(props: { update: (state: State) => void }): React.FunctionComponentElement<{}> {
-  const { search } = useLocation();
-
-  const [[, token] = []] = search
-    .split('?')
-    .reduce((acc, sides) => [...acc, ...sides.split('&')], new Array<string>())
-    .map((pair) => pair.split('='))
-    .filter(([key]) => key === 'token');
-
-  if (!token) {
-    return <Redirect to="/" />;
-  }
-
-  useEffect(() => {
-    log('initiating token exchange');
-    props.update({ session: loading(loadSession(fromNullable(token))) });
-  });
-
-  return <Redirect to="/" />;
-}
-
 function Main(props: { state: State }): React.FunctionComponentElement<{}> {
   const [state, update] = useState(props.state);
-  log('rendering the main application content');
 
   useEffect(function () {
-    log('application main effect - %o', state);
+    if (state.session.kind === 'loading') {
+      log('session request pending, waiting and sending update');
+      state.session.promise
+        .then((data) => update({ ...state, session: loaded(data) }))
+        .catch((e) => update({ ...state, session: failed([e]) }));
+    } else if (state.session.kind === 'not-asked') {
+      log('session still uninitialized, initiating request now');
+      update({ session: loading(loadSession(none())) });
+    } else if (state.session.kind === 'loaded') {
+      const { user } = state.session.data;
 
-    switch (state.session.kind) {
-      case 'loading': {
-        log('session request pending, waiting and sending update');
-        state.session.promise
-          .then((data) => update({ ...state, session: loaded(data) }))
-          .catch((e) => update({ ...state, session: failed([e]) }));
-        break;
-      }
-      case 'not-asked': {
-        log('session still uninitialized, initiating request now');
-        update({ session: loading(loadSession(none())) });
-        break;
-      }
-      case 'loaded': {
-        const { user } = state.session.data;
-
-        if (user.kind === 'some') {
-          const { id } = user.data;
-          log('session loaded w/ active user "%s"', id);
-          setContextValue('userId', id);
-        }
-        break;
-      }
-      default: {
-        log('update w/ strange session state "%s"', state.session.kind);
+      if (user.kind === 'some') {
+        const { id } = user.data;
+        log('session loaded w/ active user "%s"', id);
+        setContextValue('userId', id);
       }
     }
   });
 
   const { session } = state;
 
-  switch (session.kind) {
-    case 'not-asked':
-    case 'loading':
-      return <Loading />;
-    case 'loaded':
-      return (
-        <section data-role="loaded-session" className="main-contents">
-          <Header key="header" session={session.data} />
-          <Switch>
-            <Route extact path="/login">
-              <Routes.Login session={session.data} />
-            </Route>
-            <Route extact path="/home">
-              <Routes.Home session={session.data} />
-            </Route>
-            <Route extact path="/poll-lobby/:id">
-              <Routes.PollLobby session={session.data} />
-            </Route>
-            <Route extact path="/join-lobby/:id">
-              <Routes.JoinLobby session={session.data} />
-            </Route>
-            <Route extact path="/lobbies/:id/new-game">
-              <Routes.NewGame session={session.data} />
-            </Route>
-            <Route extact path="/lobbies/:lobbyId/games/:gameId">
-              <Routes.Game session={session.data} />
-            </Route>
-            <Route extact path="/lobbies/:id">
-              <Routes.Lobby session={session.data} />
-            </Route>
-            <Route extact path="/new-lobby">
-              <Routes.NewLobby session={session.data} />
-            </Route>
-            <Route>
-              <Redirect to="/login" />
-            </Route>
-          </Switch>
-        </section>
-      );
-    case 'failed':
-      log('unable to load session %o', session.errors);
-      return <ApplicationError errors={session.errors} />;
+  if (session.kind === 'not-asked' || session.kind === 'loading') {
+    return <Loading />;
+  } else if (session.kind === 'failed') {
+    return <ApplicationError errors={session.errors} />;
   }
+
+  return (
+    <section data-role="loaded-session" className="main-contents">
+      <Header key="header" session={session.data} />
+      <Switch>
+        <Route extact path="/login">
+          <Routes.Login session={session.data} />
+        </Route>
+        <Route extact path="/home">
+          <Routes.Home session={session.data} />
+        </Route>
+        <Route extact path="/poll-lobby/:id">
+          <Routes.PollLobby session={session.data} />
+        </Route>
+        <Route extact path="/join-lobby/:id">
+          <Routes.JoinLobby session={session.data} />
+        </Route>
+        <Route extact path="/lobbies/:id/new-game">
+          <Routes.NewGame session={session.data} />
+        </Route>
+        <Route extact path="/lobbies/:lobbyId/games/:gameId">
+          <Routes.Game session={session.data} />
+        </Route>
+        <Route extact path="/lobbies/:id">
+          <Routes.Lobby session={session.data} />
+        </Route>
+        <Route extact path="/new-lobby">
+          <Routes.NewLobby session={session.data} />
+        </Route>
+        <Route>
+          <Redirect to="/login" />
+        </Route>
+      </Switch>
+    </section>
+  );
 }
 
 // The App componet inititalizes it's sesison state to `not-asked`, letting either the `Main` component **or** the
@@ -133,19 +87,14 @@ function App(): React.FunctionComponentElement<{}> {
   const [state, update] = useState(init());
 
   useEffect(() => {
-    log('application render, session currently "%s"', state.session.kind);
-
-    if (state.session.kind === 'loading') {
-      log('session in a loading state, queing updates on resolution');
-      state.session.promise
-        .then((sesion) => update({ ...state, session: loaded(sesion) }))
-        .catch((error) => update({ ...state, session: failed([error]) }));
+    if (state.session.kind !== 'loading') {
       return;
     }
 
-    if (state.session.kind === 'loaded') {
-      log('session loaded, ');
-    }
+    log('session in a loading state, queing updates on resolution');
+    state.session.promise
+      .then((sesion) => update({ ...state, session: loaded(sesion) }))
+      .catch((error) => update({ ...state, session: failed([error]) }));
   });
 
   return (
